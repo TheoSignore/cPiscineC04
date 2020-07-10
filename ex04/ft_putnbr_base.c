460067a01f07e4ea8aec5f57acc1aab8c647f96a
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-10 16:55:19 +0200

    ex04 in progress

---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/10 15:04:57 by tsignore          #+#    #+#             */
+/*   Updated: 2020/07/10 16:54:45 by tsignore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		base_iscorrect(char *base)
+{
+	int i;
+	int j;
+
+	if (base[0] == '\0' || base[1] == '\0')
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (base[i])
+	{
+		j = 0;
+		while (base[j])
+		{
+			if (j != i && base[j] == base[i])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	unsigned int bs_len;
+	unsigned int n;
+
+	n = nbr;
+	if (base_iscorrect(base))
+	{
+		bs_len = ft_strlen(base);
+		if (n < 0)
+		{
+			ft_putchar('-');
+			n *= -1;
+		}	
+		if (n >= bs_len)
+		{
+			ft_putnbr_base((n / bs_len), base);
+			ft_putchar(base[n % bs_len]);
+		}
+		else if (n < bs_len)
+			ft_putchar(base[n]);
+	}		
+}
